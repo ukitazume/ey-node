1473be73bb92ee95b0dd021f65a09e4dd855dda9
Yu Kitazume <u.kitazume@gmail.com>
2014-10-03 12:12:15 +0900

    use 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.get('/', function(req, res){
   res.send('hello world');
 });
-var port = process.env.NODE_PORt || 80;
+var port = process.env.NODE_PORt || 8080;
 
 var server = app.listen(port, function() {
   console.log('listening on port %d', server.address().port);
