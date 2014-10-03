a569365ee53c86de8b9a3af3865444c9c498e7b0
Yu Kitazume <u.kitazume@gmail.com>
2014-10-03 12:09:48 +0900

    not harmony

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-"use strict";
-
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 app.get('/', function(req, res){
   res.send('hello world');
 });
-const port = process.env.NODE_PORt || 80;
+var port = process.env.NODE_PORt || 80;
 
-const server = app.listen(port, function() {
+var server = app.listen(port, function() {
   console.log('listening on port %d', server.address().port);
 });
 
