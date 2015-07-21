a38594ab811293e70ac8e7b6f6b6d806c8a830aa
Yu Kitazume <u.kitazume@gmail.com>
2015-07-21 10:25:48 +0900

    newrelic

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('newrelic');
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
   res.send('hello world');
 });
-var port = 8081;
+var port = 6000;
 
 var server = app.listen(port, function() {
   console.log('listening on port %d', server.address().port);
