ebefa51cef22a6835608bcb678b400000d724715
Yu Kitazume <u.kitazume@gmail.com>
2016-03-07 15:44:29 +0900

    hmm

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,3 +1,3 @@
 max_file_open = 16384
-sudo "pgrep -f god > /tmp/godpid"
-sudo "prlimit --pid $(cat /tmp/godpid) --nofile=#{max_file_open}:#{max_file_open}"
+pid = sudo "pgrep -f god"
+sudo "prlimit --pid #{pid} --nofile=#{max_file_open}:#{max_file_open}"
